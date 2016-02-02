c300cbda06bd2c18251a89339288d2b89cc988e3
Michele Mazzucco <hello@michelemazzucco.it>
2016-02-02 13:08:36 +0100

    new task for css lint

---
+++ gulpfile.js/tasks/cssLint.js
@@ -0,0 +1,22 @@
+'use strict';
+var config = require('../config'),
+    notifyError = require('../lib/notifyError');
+
+var gulp = require('gulp'),
+    postcss = require('gulp-postcss'),
+    scss = require("postcss-scss");
+
+// PostCSS plugins
+var reporter = require('postcss-reporter'),
+    stylelint = require('stylelint');
+
+gulp.task('cssLint', function() {
+  
+  var plugins = [
+      stylelint(),
+      reporter({clearMessages: true})
+  ];
+
+  gulp.src(config.dir.src + '/scss/**/*.scss')
+    .pipe(postcss(plugins, {syntax: scss}))
+});
