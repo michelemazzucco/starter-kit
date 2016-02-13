d9d9f6491269b1c4b6de17ccefbc9d6eed9e4080
Michele Mazzucco <hello@michelemazzucco.it>
2016-02-13 14:42:46 +0100

    add notify and plumber in bundle

--- gulpfile.js/tasks/bundle.js
+++ gulpfile.js/tasks/bundle.js
@@ -1,17 +1,19 @@
 'use strict';
-var config = require('../config');
+var config = require('../config'),
+    notifyError = require('../lib/notifyError');
 
 var gulp = require('gulp'),
+    notify = require('gulp-notify'),
+    plumber = require('gulp-plumber'),
     size = require('gulp-size'),
     uglify = require('gulp-uglify'),
-    concat = require('gulp-concat'),
-    browserSync = require('browser-sync');
+    concat = require('gulp-concat');
 
 gulp.task('bundle', function() {
   gulp.src(config.src.js)
+    .pipe(plumber({errorHandler: notifyError}))
     .pipe(concat('bundle.js'))
     .pipe(uglify({mangle: false, compress:true}))
     .pipe(size(config.tasks.size.opts))
     .pipe(gulp.dest(config.dest.js))
-    .pipe(browserSync.stream())
 });
