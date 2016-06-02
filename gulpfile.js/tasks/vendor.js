b2ab8a77fe7beb1c1e44a100d7b084d3a6ad17b0
Michele Mazzucco <hello@michelemazzucco.it>
2016-06-02 17:05:32 +0200

    add uglify to vendor

--- gulpfile.js/tasks/vendor.js
+++ gulpfile.js/tasks/vendor.js
@@ -8,6 +8,7 @@ var gulp = require('gulp'),
 gulp.task('vendor', function() {
   gulp.src(config.tasks.vendor.src)
     .pipe(concat('vendor.js'))
+    .pipe(uglify({mangle: false, compress:true}))
     .pipe(size(config.tasks.size.opts))
     .pipe(gulp.dest(config.dest.js))
 });
